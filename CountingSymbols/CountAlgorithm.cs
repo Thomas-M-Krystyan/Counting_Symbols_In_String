3dd025ec5737075f5508d18bf36db6f27c39db80
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-10-08 19:21:31 +0200

    Upload count symbols algorithm for string from the hard drive

---
+++ CountingSymbols/CountAlgorithm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountingSymbols
+{
+    internal static class CountAlgorithm
+    {
+        // Starts the algorithm
+        internal static void Process(string textData, bool caseSensitive = true)
+        {
+            Dictionary<char, int> countedLettersDict = ReturnCounted(textData, caseSensitive);
+            Dictionary<char, int> sortedCountedLettersDict = ReturnSorted(countedLettersDict);
+            ToString(textData, sortedCountedLettersDict);
+        }
+
+        // Counts the occurrence of every digit, letter or special symbol in the given string
+        private static Dictionary<char, int> ReturnCounted(string textData, bool caseSensitive)
+        {
+            Dictionary<char, int> countedLettersDict = new Dictionary<char, int>();
+
+            foreach (char character in textData)  // e.g. word or message
+            {
+                // Normal symbol or converted to Uppercase --> which is depends on [caseSensitive] flag
+                char characterToSave = caseSensitive ? character : Convert.ToChar(character.ToString().ToUpper());
+
+                if (!countedLettersDict.ContainsKey(characterToSave))
+                {
+                    countedLettersDict.Add(characterToSave, 1);  // Add new symbol to dictionary and set it's occurrence as 1
+                }
+                else
+                {
+                    countedLettersDict.TryGetValue(characterToSave, out int value);
+                    countedLettersDict[characterToSave] = value + 1;
+                }
+            }
+
+            return countedLettersDict;
+        }
+
+        // Sorts dictionary KeyValuePair by the key (set higher priority to the lowercase letters)
+        private static Dictionary<char, int> ReturnSorted(Dictionary<char, int> countedLettersDict)
+        {
+            // Uppercase letters has the higher priority, so there's a need to converting every letter to lowercase
+            Dictionary<char, int> sortedCountedLettersDict = countedLettersDict.OrderBy(pair => pair.Key.ToString().
+                ToLower()).ToDictionary(pair => pair.Key, pair => pair.Value);
+
+            return sortedCountedLettersDict;
+        }
+
+        // Displays sorted and counted results as the vertical list of elements
+        private static void ToString(string textData, Dictionary<char, int> sortedCountedLettersDict)
+        {
+            Console.WriteLine("Number of symbols in the text:");
+            Console.WriteLine($"\"{textData}\"\n");
+            foreach (KeyValuePair<char, int> pair in sortedCountedLettersDict)
+            {
+                Console.WriteLine($"  \"{pair.Key}\": {pair.Value}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CountingSymbols/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CountingSymbols
+{
+    internal static class Program
+    {
+        internal static void Main(string[] args)
+        {
+            const string textData = @"[SerializableAttribute] public class SortedDictionary<TKey, TValue> " +
+                                    ": IDictionary<TKey, TValue>, ICollection<KeyValuePair<TKey, TValue>>, " +
+                                    "IEnumerable<KeyValuePair<TKey, TValue>>, IEnumerable, IDictionary, ICollection, " +
+                                    "IReadOnlyDictionary<TKey, TValue>, IReadOnlyCollection<KeyValuePair<TKey, TValue>>";
+
+            CountAlgorithm.Process(textData);  // Case sensitive mode (default: true)
+        }
+    }
+}
